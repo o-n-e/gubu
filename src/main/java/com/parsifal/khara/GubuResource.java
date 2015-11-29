742322f0185f6d91b54ced0aa30d80b0f206c662
o-n-e <stewart.barron@mac.com>
2015-11-29 18:27:35 +0000

    Initial Commit

---
+++ src/main/java/com/parsifal/khara/GubuResource.java
@@ -0,0 +1,12 @@
+package com.parsifal.khara;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
+@Path("/moment")
+@Produces(MediaType.APPLICATION_JSON)
+public class GubuResource {
+
+}
---
+++ src/main/java/com/parsifal/khara/GubuService.java
@@ -0,0 +1,24 @@
+package com.parsifal.khara;
+
+
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Configuration;
+import com.yammer.dropwizard.config.Environment;
+
+public class GubuService extends Service<Configuration> {
+
+    public static void main(String[] args) throws Exception {
+        new GubuService().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<Configuration> bootstrap) {
+
+    }
+
+    @Override
+    public void run(Configuration configuration, Environment environment) throws Exception {
+        environment.addResource(new GubuResource());
+    }
+}
